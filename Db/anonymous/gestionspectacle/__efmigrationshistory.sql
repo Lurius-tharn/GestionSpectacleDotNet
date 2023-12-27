f39ae0bffcfa118ab6f8eea71a935e43d1b159cb
Davenese <sajous.davenese@gmail.com>
2023-12-27 15:31:01 +0100

    AJOUT SQL

--- Db/anonymous/gestionspectacle/__efmigrationshistory.sql
+++ Db/anonymous/gestionspectacle/__efmigrationshistory.sql
@@ -1,8 +1,1 @@
-create table __efmigrationshistory
-(
-    MigrationId    varchar(150) not null
-        primary key,
-    ProductVersion varchar(32)  not null
-);
-
 INSERT INTO gestionspectacle.__efmigrationshistory (MigrationId, ProductVersion) VALUES ('20231217134615_HistoriqueBillet', '6.0.25');
--- Db/anonymous/gestionspectacle/billet.sql
+++ Db/anonymous/gestionspectacle/billet.sql
@@ -1,18 +1,6 @@
-create table billet
-(
-    Id            int auto_increment
-        primary key,
-    idSpectacle   int         null,
-    idUtilisateur int         null,
-    statut        varchar(36) null,
-    numeroBillet  int         null,
-    constraint Billet_spectacle_null_fk
-        foreign key (idSpectacle) references spectacle (id),
-    constraint Billet_utilisateur_null_fk
-        foreign key (idUtilisateur) references utilisateur (id)
-);
-
-INSERT INTO gestionspectacle.billet (Id, idSpectacle, idUtilisateur, statut, numeroBillet) VALUES (12, 12, 10, 'Reserve', 5);
-INSERT INTO gestionspectacle.billet (Id, idSpectacle, idUtilisateur, statut, numeroBillet) VALUES (13, 13, 10, 'Reserve', 1);
-INSERT INTO gestionspectacle.billet (Id, idSpectacle, idUtilisateur, statut, numeroBillet) VALUES (14, 13, 10, 'Reserve', 2);
-INSERT INTO gestionspectacle.billet (Id, idSpectacle, idUtilisateur, statut, numeroBillet) VALUES (15, 13, 10, 'Reserve', 3);
+INSERT INTO gestionspectacle.billet (Id, idSpectacle, idUtilisateur, statut, numeroBillet) VALUES (16, 14, 15, 'Reserve', 1);
+INSERT INTO gestionspectacle.billet (Id, idSpectacle, idUtilisateur, statut, numeroBillet) VALUES (17, 14, 15, 'Reserve', 2);
+INSERT INTO gestionspectacle.billet (Id, idSpectacle, idUtilisateur, statut, numeroBillet) VALUES (18, 14, 15, 'Reserve', 3);
+INSERT INTO gestionspectacle.billet (Id, idSpectacle, idUtilisateur, statut, numeroBillet) VALUES (22, 14, 10, 'Reserve', 1);
+INSERT INTO gestionspectacle.billet (Id, idSpectacle, idUtilisateur, statut, numeroBillet) VALUES (23, 14, 10, 'Reserve', 2);
+INSERT INTO gestionspectacle.billet (Id, idSpectacle, idUtilisateur, statut, numeroBillet) VALUES (24, 14, 10, 'Reserve', 3);
